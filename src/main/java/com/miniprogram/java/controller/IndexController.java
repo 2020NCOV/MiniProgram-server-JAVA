1f75d8c05e5c391c1443ecc156e615edab6f430b
msq0313 <513299488@qq.com>
2020-03-08 23:18:40 +0800

    springboot frame

---
+++ src/main/java/com/miniprogram/java/controller/IndexController.java
@@ -0,0 +1,14 @@
+package com.miniprogram.java.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+
+    @RequestMapping(value = "/index", method = RequestMethod.GET)
+    public String index() {
+        return "MiniProgram-server-JAVA";
+    }
+}
